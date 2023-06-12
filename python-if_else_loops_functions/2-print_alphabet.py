b65056dd6f0782547cf815a61c3d8af69d35bbd3
raed <raed.da3k@gmail.com>
2023-06-12 11:36:47 +0100

    add

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-for letter in alphabet:
-    if letter not in ['q', 'e']:
-        print(letter, end='')
+
+for c in range(97, 123):
+    if chr(c) not in ['q', 'e']:
+        print('{}' .format(chr(c)), end='')
