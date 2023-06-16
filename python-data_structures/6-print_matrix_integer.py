f703d472313d4fc6e7a273c09c7aa80f0ec5483f
raed <raed.da3k@gmail.com>
2023-06-16 11:46:05 +0100

    switch row with line

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for x in row:
-            if x != row[-1]:
+    for line in matrix:
+        for x in line:
+            if x != line[-1]:
                 print("{:d}".format(x), end=" ")
             else:
                 print("{:d}".format(x), end=" ")
