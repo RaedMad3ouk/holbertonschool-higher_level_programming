a4e1acd139336f532f3bee53fa635150c2b49f38
raed <raed.da3k@gmail.com>
2023-07-07 12:51:02 +0100

    fix

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""Define Square class"""
+
 
 class Square:
+    """define fields and methods of Square"""
+
     def __init__(self, size):
+        """ Square Constructor.
+    args:
+        size:size of the square
+    """
+
         self.__size = size
