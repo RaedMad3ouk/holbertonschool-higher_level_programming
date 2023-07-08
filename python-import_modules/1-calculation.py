0b611e410e54bd6ced06a0117488a9c7ee0fb423
raed <raed.da3k@gmail.com>
2023-07-08 14:53:41 +0100

    print list

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-from re import X
 
-
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    from calculator_1 import add, mul, sub, div
     a = 10
     b = 5
-    add_res = add(a, b)
-    mul_res = mul(a, b)
-    sub_res = sub(a, b)
-    div_res = div(a, b)
-    print("{} + {} = {}".format(a, b, add_res))
-    print("{} - {} = {}".format(a, b, sub_res))
-    print("{} * {} = {}".format(a, b, mul_res))
-    print("{} / {} = {}".format(a, b, div_res))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
