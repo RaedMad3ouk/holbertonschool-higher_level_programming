9d5e9c387a149d24834c79ddce6f9d3896c6679e
raed <raed.da3k@gmail.com>
2023-07-26 13:10:21 +0100

    base geo

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Create an empty class named BaseGeometry."""
+
+
+class BaseGeometry:
+    """
+    Methods:
+        area(self)
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
