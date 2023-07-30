5bb3ba97aaef712662611865e1a318aebf726116
raed <raed.da3k@gmail.com>
2023-07-30 11:33:58 +0100

    infint loop

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 import sys
+
 if __name__ == "__main__":
     args = sys.argv[1:]
-    arg_n = len(args)
-
-    total = 0
-    for arg in args:
-        total += int(arg)
+    total = sum(int(arg) for arg in args)
     print(total)
