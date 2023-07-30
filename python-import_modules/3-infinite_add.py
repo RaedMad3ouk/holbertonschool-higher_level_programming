2d9d38087ce1b1628e481573ff40eed08bb2acb9
raed <raed.da3k@gmail.com>
2023-07-30 11:37:13 +0100

    infint add

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-import sys
-
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    total = sum(int(arg) for arg in args)
-    print(total)
+    import sys
+    ar = sys.argv
+    suma = 0
+    if len(ar) == 1:
+        print("0")
+        exit()
+    for i in range(1, len(ar)):
+        x = int(ar[i])
+        suma += x
+    print(suma)
