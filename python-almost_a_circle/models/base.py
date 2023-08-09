ca9ff93afa84cd8216e61217c81907b670c71ce0
raed <raed.da3k@gmail.com>
2023-08-09 06:16:28 +0100

    edit base for advanced prototype

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,79 +1,103 @@
 #!/usr/bin/python3
-"""Unittest class TestBase
+""" Base class model
 """
-import unittest
-from models.base import Base
 
 
-class Test(unittest.TestCase):
-    """ Class for testing Base
+class Base:
+    """ Base class
     """
-    def setUp(self):
-        Base._reset_nb_objects()
+    __nb_objects = 0
 
-    def test_id_assignment(self):
-        """ Tests first id assignment
+    def __init__(self, id=None):
+        """ Class constructor
         """
-        b1 = Base()
-        self.assertEqual(b1.id, 1)
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
-    def test_id_increment(self):
-        """ Tests id increment
+    @staticmethod
+    def from_json_string(json_string):
+        """ Returns list of JSON string representation
         """
-        b1 = Base()
-        b2 = Base()
-        self.assertEqual(b2.id, 2)
+        from json import loads as l
+        newlist = []
+        if json_string is None or len(json_string) < 1:
+            return newlist
+        else:
+            newlist = l(json_string)
+            return newlist
 
-    def test_id_override(self):
-        """ Tests if id arg overrides increment
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ Returns JSON string rep of list_dictionaries
         """
-        b1 = Base()
-        b2 = Base()
-        self.assertEqual(b2.id, 2)
-        b4 = Base(12)
-        self.assertEqual(b4.id, 12)
+        import json
+        if not list_dictionaries:
+            return ("[]")
+        else:
+            return json.dumps(list_dictionaries)
 
-    def test_id_reverts(self):
-        """ Tests if id increment reverts after override
-        """
-        b1 = Base()
-        b2 = Base()
-        self.assertEqual(b2.id, 2)
-        b3 = Base(33)
-        self.assertEqual(b3.id, 33)
-        b4 = Base()
-        self.assertEqual(b4.id, 3)
-            def create(cls, **dictionary):
+    @classmethod
+    def create(cls, **dictionary):
         """ Returns an instance with all attrs set
         """
-        pass
-        dummy = cls(width=1, height=1, x=0, y=0, id=None)
-        return(dummy.update(**dictionary))
+        dummy = cls(width=1, height=1, x=0, y=0)
+        dummy.update(**dictionary)
+        return dummy
 
     @classmethod
     def load_from_file(cls):
-@@ -55,13 +56,22 @@ def load_from_file(cls):
+        """ Returns a list of instances
+        """
+        pass
+
+    @classmethod
     def save_to_file(cls, list_objs):
         """ Writes JSON string rep of list_objs to file
         """
-        import json
-        filename = cls.__name__
+        fm = cls.__name__
+        empty = []
         dictlist = []
         dictlist2 = []
-        for x in list_objs:
-            dictlist.append(vars(x))
-        with open("{}.json".format(filename), mode='w', encoding='utf-8') as f:
-            f.write(Base.to_json_string(dictlist))
-            newf = (Base.to_json_string(dictlist))
-            newf = (newf.replace('_Rectangle__', ''))
-            f.write(newf)
-        # filename = cls.__name__
-        # dictlist = []
-        # dictlist2 = []
-        # for x in list_objs:
-        #     dictlist.append(vars(x))
-        # with open("{}.json".format(filename), mode='w', encoding='utf-8') as
-        #     f.write(Base.to_json_string(dictlist))
+        if not list_objs:
+            with open("{}.json".format(fm), mode='w', encoding='utf-8') as f:
+                f.write(Base.to_json_string(empty))
+        else:
+            for x in list_objs:
+                dictlist.append(vars(x))
+            with open("{}.json".format(fm), mode='w', encoding='utf-8') as f:
+                newf = (Base.to_json_string(dictlist))
+                newf = (newf.replace('_Rectangle__', ''))
+                f.write(newf)
+
+    @classmethod
+    def load_from_file(cls):
+        """ Returns list of instances
+        """
+        pass
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """ Serializes to csv
+        """
+        pass
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ Deserializes from csv
+        """
+        pass
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """ Draws all recs and squares
+        """
+        pass
 
     @classmethod
     def _reset_nb_objects(cls):
+        """ For testing only, hook in setUp to reset class-level id num
+        """
+        cls.__nb_objects = 0
