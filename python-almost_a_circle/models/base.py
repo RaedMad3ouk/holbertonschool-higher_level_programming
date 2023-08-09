286befadff74576107a0fabe462cab42be734f37
raed <raed.da3k@gmail.com>
2023-08-09 06:14:37 +0100

    update base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,103 +1,45 @@
 #!/usr/bin/python3
-""" Base class model
+"""Unittest class TestBase
 """
+import unittest
+from models.base import Base
 
 
-class Base:
-    """ Base class
+class Test(unittest.TestCase):
+    """ Class for testing Base
     """
-    __nb_objects = 0
+    def setUp(self):
+        Base._reset_nb_objects()
 
-    def __init__(self, id=None):
-        """ Class constructor
+    def test_id_assignment(self):
+        """ Tests first id assignment
         """
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
 
-    @staticmethod
-    def from_json_string(json_string):
-        """ Returns list of JSON string representation
+    def test_id_increment(self):
+        """ Tests id increment
         """
-        from json import loads as l
-        newlist = []
-        if json_string is None or len(json_string) < 1:
-            return newlist
-        else:
-            newlist = l(json_string)
-            return newlist
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
 
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        """ Returns JSON string rep of list_dictionaries
+    def test_id_override(self):
+        """ Tests if id arg overrides increment
         """
-        import json
-        if not list_dictionaries:
-            return ("[]")
-        else:
-            return json.dumps(list_dictionaries)
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        b4 = Base(12)
+        self.assertEqual(b4.id, 12)
 
-    @classmethod
-    def create(cls, **dictionary):
-        """ Returns an instance with all attrs set
+    def test_id_reverts(self):
+        """ Tests if id increment reverts after override
         """
-        dummy = cls(width=1, height=1, x=0, y=0)
-        dummy.update(**dictionary)
-        return dummy
-
-    @classmethod
-    def load_from_file(cls):
-        """ Returns a list of instances
-        """
-        pass
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """ Writes JSON string rep of list_objs to file
-        """
-        fm = cls.__name__
-        empty = []
-        dictlist = []
-        dictlist2 = []
-        if not list_objs:
-            with open("{}.json".format(fm), mode='w', encoding='utf-8') as f:
-                f.write(Base.to_json_string(empty))
-        else:
-            for x in list_objs:
-                dictlist.append(vars(x))
-            with open("{}.json".format(fm), mode='w', encoding='utf-8') as f:
-                newf = (Base.to_json_string(dictlist))
-                newf = (newf.replace('_Rectangle__', ''))
-                f.write(newf)
-
-    @classmethod
-    def load_from_file(cls):
-        """ Returns list of instances
-        """
-        pass
-
-    @classmethod
-    def save_to_file_csv(cls, list_objs):
-        """ Serializes to csv
-        """
-        pass
-
-    @classmethod
-    def load_from_file_csv(cls):
-        """ Deserializes from csv
-        """
-        pass
-
-    @staticmethod
-    def draw(list_rectangles, list_squares):
-        """ Draws all recs and squares
-        """
-        pass
-
-    @classmethod
-    def _reset_nb_objects(cls):
-        """ For testing only, hook in setUp to reset class-level id num
-        """
-        cls.__nb_objects = 0
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        b3 = Base(33)
+        self.assertEqual(b3.id, 33)
+        b4 = Base()
+        self.assertEqual(b4.id, 3)
