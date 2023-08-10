b97d8ddb30811b953e2537480b7bd8ab25af6049
raed <raed.da3k@gmail.com>
2023-08-10 13:21:42 +0100

    add

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,33 +1,38 @@
 #!/usr/bin/python3
 """
-     Module for matrix_divided functions
-
-
+This is the "2-matrix_divided" module.
+The 2-matrix_divided module functions: matrix_divided(a, b).
 """
 
 
 def matrix_divided(matrix, div):
-    """ Divide matrix function
-
     """
-    count = len(matrix)
-    errmess = "matrix must be a matrix (list of lists) of integers/floats"
-
-    if not isinstance(div, (int, float)):
+    matrix_divided: divides all elements of a matrix
+    Args:
+        matrix (int, float):  list of lists of integers or floats
+        div (int): divident
+    Returns:
+        int
+    """
+    size = None
+    if type(matrix) is not list:
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+    for l in matrix:
+        if type(l) is not list:
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+        if size is None:
+            size = len(l)
+        elif size != len(l):
+            raise TypeError("Each row of the matrix must have the same size")
+        for i in l:
+            if type(i) is not int and type(i) is not float:
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) of integers/floats")
+    if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     if div == 0:
-        raise TypeError("division by zero")
-    if not isinstance(matrix, list):
-        raise TypeError(errmess)
-    while count > -1:
-        if len(matrix[0]) != len(matrix[count-1]):
-            raise TypeError("Each row of the matrix must have the same size")
-        count -= 1
+        raise ZeroDivisionError("division by zero")
+    return [[round(i / div, 2) for i in l] for l in matrix]
 
-    newmatrix2 = []
-    for x in range(len(matrix)):
-        newmatrix = []
-        for y in range(len(matrix[x])):
-            newmatrix.append(round(matrix[x][y] / div, 2))
-        newmatrix2.append(newmatrix)
-    return newmatrix2
