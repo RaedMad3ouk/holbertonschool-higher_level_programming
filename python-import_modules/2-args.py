12faee912e74bb3f4d0727993a80c101b196535d
raed <raed.da3k@gmail.com>
2023-08-10 13:05:09 +0100

    add

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import sys
-if __name__ == "__main__":
-    args = sys.argv[1:]
-    arg_n = len(args)
-    if arg_n == 0:
+if __name__ == '__main__':
+    from sys import argv
+    argc = len(argv)
+    if argc < 2:
         print("0 arguments.")
-    else:
-        print("{} {}:".format(arg_n, "arguments" if arg_n > 1 else "argument"))
-        i = 0
-        for arg in sys.argv[1:]:
-            i += 1
-            print("{}: {}".format(i, arg))
+    for i in range(1, argc):
+        if argc < 3:
+            print("1 argument:")
+            print("1: {}".format(argv[1]))
+        else:
+            if i == 1:
+                print("{} arguments:".format(argc-1))
+            print("{}: {}".format(i, argv[i]))
